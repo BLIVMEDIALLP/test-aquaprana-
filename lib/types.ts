b49b0c2ef79f374b77f3a18d8c0fd19bbe95412a
BLIVMEDIALLP <a38169794@gmail.com>
2026-03-30 13:52:33 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,31 +1,3 @@
-export interface Pond {
-  id: string
-  name: string
-  area_acres?: number
-}
-
-export interface CropCycle {
-  id: string
-  pond_id: string
-  species: string
-  stocking_date: string
-  stocking_density: number
-  harvest_window_start?: string
-  harvest_window_end?: string
-}
-
-export interface PondLog {
-  id: string
-  cycle_id: string
-  observed_at: string
-  do_mgl?: number
-  ph?: number
-  ammonia_mgl?: number
-  temp_c?: number
-  salinity_ppt?: number
-  calcium_mgl?: number
-  magnesium_mgl?: number
-  potassium_mgl?: number
-  mortality_count?: number
-  biomass_kg?: number
-}
+export type Pond = any;
+export type CropCycle = any;
+export type PondLog = any;
